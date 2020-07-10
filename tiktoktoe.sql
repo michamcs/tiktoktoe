55271cea866e397224eb05da5560c801f738712e
michaelmarcus <marcus.mika@gmail.com>
2020-07-10 10:12:42 +0300

    DB connection and creation

--- tiktoktoe.sql
+++ tiktoktoe.sql
@@ -1,59 +1,52 @@
-CREATE TABLE `TikTokPost` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `user_id` int,
-  `number_of_likes` int,
-  `number_of_share` int,
-  `post_text` varchar(255),
-  `url` varchar(255)
-);
-
-CREATE TABLE `PostHashtags` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `hashtag_id` int,
-  `post_id` int
-);
-
-CREATE TABLE `UserBioHashtags` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `hashtag_id` int,
-  `user_id` int
-);
-
-CREATE TABLE `AllHashtags` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `hashtag` varchar(255)
-);
-
-CREATE TABLE `Songs` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `post_id` int,
-  `song_id` int
-);
-
-CREATE TABLE `AllSongs` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `song_name` varchar(255)
-);
-
 CREATE TABLE `TikTokUsers` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `user_name` varchar(255),
+  `id` int PRIMARY KEY,
+  `user_id` varchar(255),
   `number_of_followers` int,
   `number_of_following` int,
   `number_of_profile_likes` int,
   `bio_text` varchar(255)
 );
 
-ALTER TABLE `PostHashtags` ADD FOREIGN KEY (`post_id`) REFERENCES `TikTokPost` (`id`);
+CREATE TABLE `TikTokPost` (
+  `id` int PRIMARY KEY,
+  `user_id` varchar(255),
+  `number_of_likes` int,
+  `number_of_share` int,
+  `post_text` varchar(255),
+  `url` varchar(255),
+  FOREIGN KEY (user_id) REFERENCES TikTokUsers(id)
+);
 
-ALTER TABLE `PostHashtags` ADD FOREIGN KEY (`hashtag_id`) REFERENCES `AllHashtags` (`id`);
+CREATE TABLE `AllHashtags` (
+  `id` int PRIMARY KEY,
+  `hashtag` varchar(255)
+);
 
-ALTER TABLE `UserBioHashtags` ADD FOREIGN KEY (`hashtag_id`) REFERENCES `AllHashtags` (`id`);
+CREATE TABLE `PostHashtags` (
+  `id` int PRIMARY KEY,
+  `post_id` varchar(255),
+  `hashtag_id` varchar(255),
+  FOREIGN KEY (post_id) REFERENCES TikTokPost(id),
+  FOREIGN KEY (hashtag_id) REFERENCES AllHashtags(id)
+);
 
-ALTER TABLE `UserBioHashtags` ADD FOREIGN KEY (`user_id`) REFERENCES `TikTokUsers` (`id`);
+CREATE TABLE `UserBioHashtags` (
+  `id` int PRIMARY KEY,
+  `user_id` varchar(255),
+  `hashtag_id` varchar(255),
+  FOREIGN KEY (user_id) REFERENCES TikTokUsers(id),
+  FOREIGN KEY (hashtag_id) REFERENCES AllHashtags(id)
+);
 
-ALTER TABLE `TikTokPost` ADD FOREIGN KEY (`id`) REFERENCES `Songs` (`post_id`);
+CREATE TABLE `AllSongs` (
+  `id` int PRIMARY KEY,
+  `song_name` varchar(255)
+);
 
-ALTER TABLE `Songs` ADD FOREIGN KEY (`song_id`) REFERENCES `AllSongs` (`id`);
-
-ALTER TABLE `TikTokPost` ADD FOREIGN KEY (`user_id`) REFERENCES `TikTokUsers` (`id`);
+CREATE TABLE `Songs` (
+  `id` int PRIMARY KEY,
+  `post_id` int,
+  `song_id` int,
+  FOREIGN KEY (post_id) REFERENCES TikTokPosts(id),
+  FOREIGN KEY (song_id) REFERENCES AllSongs(id)
+);
