473844ae97b6cd07754e93ce618946cd1b8808b9
tdubnov <tdubnov@gmail.com>
2020-07-26 11:55:01 -0700

    adding keys for twitter api

--- app/env/conf.py
+++ app/env/conf.py
@@ -10,3 +10,5 @@ URL_TO_SCROLL = "https://www.tiktok.com/trending"
 DB_NAME = "tiktoktoe.db"
 SQL_FILE = "tiktoktoe.sql"
 LOG_FILE = 'tiktoktoe_logs.log'
+CLIENT_KEY = 'nOy0Wqk6SnvCCUl4QRcfFvK98'
+CLIENT_SECRET = 'LqkyT2VKSDENhI9LPs2xojNoEfgINGDmV4Sb8IQCYGI8Bct1B3'
