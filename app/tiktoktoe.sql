42798df3ca24ec45cb10b65aaf28bcc2639672d8
michaelmarcus <marcus.mika@gmail.com>
2020-07-31 13:18:40 +0300

    New try encoding

--- app/tiktoktoe.sql
+++ app/tiktoktoe.sql
@@ -1,10 +1,10 @@
 CREATE TABLE TikTokUsers (
   user_id INT PRIMARY KEY AUTO_INCREMENT,
-  user_name VARCHAR(255),
+  user_name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   number_of_followers BIGINT,
   number_of_following BIGINT,
   number_of_likes BIGINT,
-  bio_text VARCHAR(255)
+  bio_text VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 );
 
 CREATE TABLE TikTokPost (
@@ -13,14 +13,14 @@ CREATE TABLE TikTokPost (
   number_of_likes BIGINT,
   number_of_share BIGINT,
   number_of_comments BIGINT,
-  post_text VARCHAR(255),
-  url VARCHAR(255),
+  post_text VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  url VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   FOREIGN KEY (user_id) REFERENCES TikTokUsers(user_id)
 );
 
 CREATE TABLE AllHashtags (
   hash_id INT PRIMARY KEY AUTO_INCREMENT,
-  hashtag VARCHAR(255)
+  hashtag VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 );
 
 CREATE TABLE PostHashtags (
@@ -41,7 +41,7 @@ CREATE TABLE UserBioHashtags (
 
 CREATE TABLE AllSongs (
   song_id INT PRIMARY KEY AUTO_INCREMENT,
-  song_name VARCHAR(255)
+  song_name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 );
 
 CREATE TABLE Songs (
@@ -55,8 +55,8 @@ CREATE TABLE Songs (
 CREATE TABLE Tweets (
     tweet_id INT PRIMARY KEY AUTO_INCREMENT,
     hash_id INT,
-    twitter_user VARCHAR(255),
-    text VARCHAR(255),
-    url VARCHAR(255),
+    twitter_user VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+    text VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+    url VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
     FOREIGN KEY (hash_id) REFERENCES AllHashtags(hash_id)
 );
